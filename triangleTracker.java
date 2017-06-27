dc58c9f9156fc7f6181620668e1673240a492599
at and sz <at@>
2017-06-27 09:24:30 -0700

    taking customer input

--- triangleTracker.java
+++ triangleTracker.java
@@ -1,6 +1,21 @@
+import java.io.Console;
+
 public class triangleTracker {
   public static void main(String[] args){
-  Triangle myTri = new Triangle(2,2,3);
+
+  Console myConsole = System.console();
+  //get side one
+  System.out.println("Enter a length for side1");
+  int side1 = Integer.parseInt(myConsole.readLine());
+  //get side two
+  System.out.println("Enter a length for side2");
+  int side2 = Integer.parseInt(myConsole.readLine());
+  //get side three
+  System.out.println("Enter a length for side3");
+  int side3 = Integer.parseInt(myConsole.readLine());
+
+  Triangle myTri = new Triangle(side1,side2,side3);
+  System.out.println("Your triangle is ");
   System.out.println(myTri.type());
   }
 }
